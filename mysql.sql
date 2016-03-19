58aabf85c3a0e245d2c10cdea06e1ced3482d653
mapleincode <llkj13579@126.com>
2016-03-20 06:00:50 +0800

    修复正则处理 url 报错

---
+++ mysql.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `jiandan_image` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `image` varchar(100) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `retry_times` tinyint(4) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `error_message` varchar(50) DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `status` (`status`),
+  KEY `update_at` (`updated_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `jiandan_crawl` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `date` varchar(50) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  `images` text,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
