6b0d0bc15f8796099fe2e3a81ac7864941abc04d
mapleincode <llkj13579@126.com>
2016-03-20 03:34:26 +0800

    防机器验证

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var CronJob = require('cron').CronJob;
 var crawl = require('./lib/crawl');
 
-new CronJob('*/5 * * * * *', function () {
+new CronJob('30 * * * * *', function () {
     crawl.done();
 }, null, true);
